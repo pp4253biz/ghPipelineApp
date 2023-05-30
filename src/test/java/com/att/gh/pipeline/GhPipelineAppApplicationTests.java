821a4b99f2511ba0395ca06816addf4539292f51
pp4253biz <parvinderpelia@hotmail.com>
2023-05-30 13:25:10 -0700

    Initial Commit

---
+++ src/test/java/com/att/gh/pipeline/GhPipelineAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.att.gh.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhPipelineAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
